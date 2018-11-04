191e65dff6c98a7398ac0d18fcd73ed8a56c8485
subhash <subhash@gmail.com>
2018-11-04 20:10:41 +0530

    first

---
+++ src/main/java/project2/SimpleService.java
@@ -0,0 +1,15 @@
+package project2;
+
+import com.google.gwt.angular.client.NgInject;
+
+@NgInject(name="simple")
+public class SimpleService {
+
+	public String greet(String name) {
+		return "Hello " + name;
+	}
+	
+	public String dismiss(String name) {
+		return "Goodbye " + name;
+	}
+}
---
+++ src/test/java/project2/MyModuleTest.java
@@ -0,0 +1,9 @@
+package project2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyModuleTest {
+
+}
